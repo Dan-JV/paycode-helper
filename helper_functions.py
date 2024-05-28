45bb95dc0a84ae0ef6c181c6ba91cca0605ed2d0
lundberg-johan12 <johan.lundberg@visma.com>
2024-05-28 21:04:54 +0200

    stuff, too much

--- helper_functions.py
+++ helper_functions.py
@@ -6,16 +6,25 @@ import json
 import streamlit as st
 
 # S3 config
-s3 = boto3.client('s3')
-template_bucket = 'paycodehelper-templates'
-processing_bucket = 'paycodehelper-processing'
-documented_bucket = 'paycodehelper-documented'
+s3 = boto3.client("s3")
+template_bucket = "paycodehelper-templates"
+processing_bucket = "paycodehelper-processing"
+documented_bucket = "paycodehelper-documented"
 lock_timeout = datetime.timedelta(minutes=30)
 
+
+@st.cache_data
+def load_streamlit_template():
+    with open("paycode_input_template.json", "r", encoding="utf-8") as file:
+        data = json.load(file)
+
+    return data
+
+
 def list_available_paycodes(bucket: str) -> list:
-    paginator = s3.get_paginator('list_objects_v2')
+    paginator = s3.get_paginator("list_objects_v2")
     response_iterator = paginator.paginate(Bucket=bucket)
-    
+
     available_paycodes = []
 
     for page in response_iterator:
@@ -29,13 +38,23 @@ def cleanup_inprocessing_bucket():
     # move objects to other bucket and delete the bucket
     available_paycodes = list_available_paycodes(bucket="paycodehelper-processing")
     for paycode in available_paycodes:
-        s3.copy_object(Bucket="paycodehelper-templates", CopySource={'Bucket': 'paycodehelper-processing', 'Key': paycode}, Key=paycode)
+        s3.copy_object(
+            Bucket="paycodehelper-templates",
+            CopySource={"Bucket": "paycodehelper-processing", "Key": paycode},
+            Key=paycode,
+        )
         s3.delete_object(Bucket="paycodehelper-processing", Key=paycode)
 
-def move_paycode_from_source_to_target(source_bucket: str, target_bucket: str, paycode: str):
-    s3.copy_object(Bucket=target_bucket, CopySource={'Bucket': source_bucket, 'Key': paycode}, Key=paycode)
-    s3.delete_object(Bucket=source_bucket, Key=paycode)
 
+def move_paycode_from_source_to_target(
+    source_bucket: str, target_bucket: str, paycode: str
+):
+    s3.copy_object(
+        Bucket=target_bucket,
+        CopySource={"Bucket": source_bucket, "Key": paycode},
+        Key=paycode,
+    )
+    s3.delete_object(Bucket=source_bucket, Key=paycode)
 
 
 def add_to_streamlit_session_state(name: str):
@@ -43,11 +62,15 @@ def add_to_streamlit_session_state(name: str):
         result = func(*args, **kwargs)
         st.session_state[name] = result
         return result
+
     return wrapper
 
 
 def pick_random_paycode_click():
-    result = get_random_paycode(source_bucket="paycodehelper-templates", target_bucket="paycodehelper-processing")
+    result = get_random_paycode(
+        source_bucket="paycodehelper-templates",
+        target_bucket="paycodehelper-processing",
+    )
     st.session_state.paycode = result
 
 
@@ -57,12 +80,14 @@ def add_to_streamlit_session_state(name: str):
             result = func(*args, **kwargs)
             st.session_state[name] = result
             return result
+
         return wrapper
+
     return decorator
 
 
 @add_to_streamlit_session_state(name="paycode")
-def get_random_paycode(source_bucket:str, target_bucket:str) -> dict:
+def get_random_paycode(source_bucket: str, target_bucket: str) -> dict:
     """Retrieves a random paycode json file from the source bucket and the moves it to processing bucket. Fianlly it deleted the file from the source bucket."""
     available_paycodes = list_available_paycodes(bucket=source_bucket)
     if not available_paycodes:
@@ -70,15 +95,18 @@ def get_random_paycode(source_bucket:str, target_bucket:str) -> dict:
     paycode = random.choice(available_paycodes)
 
     # Copy the paycode to the processing bucket
-    s3.copy_object(Bucket=target_bucket, CopySource={'Bucket': source_bucket, 'Key': paycode}, Key=paycode)
+    s3.copy_object(
+        Bucket=target_bucket,
+        CopySource={"Bucket": source_bucket, "Key": paycode},
+        Key=paycode,
+    )
 
     # Get the paycode json file from the processing bucket and load it as a dictionary
     paycode_json_string = s3.get_object(Bucket=target_bucket, Key=paycode)
     paycode_json = json.loads(paycode_json_string.get("Body").read().decode("utf-8"))
 
     # Delete the paycode from the source bucket
-    s3.delete_object(Bucket=source_bucket, Key=paycode)
+    #s3.delete_object(Bucket=source_bucket, Key=paycode)
 
-    #return the paycode json file
+    # return the paycode json file
     return paycode_json
-
