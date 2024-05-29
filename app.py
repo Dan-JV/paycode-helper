042cdfef6f6e646757420dbd5087ce7d9eaa4b30
lundberg-johan12 <johan.lundberg@visma.com>
2024-05-29 17:33:28 +0200

    srroy dan

--- app.py
+++ app.py
@@ -3,22 +3,31 @@ from streamlit_tags import st_tags
 
 # must be the first line called to avoid it being called twice
 st.set_page_config(
-    page_title="guide buddy",
+    page_title="Data Entry Form",
     page_icon=None,
     layout="wide",
-    initial_sidebar_state="collapsed",
+    initial_sidebar_state="expanded",
 )
+
+st.title("Data Entry Form")
+
+from aws_helper_functions import (
+    move_paycode_from_source_to_target,
+)
+
+from streamlit_utils import load_streamlit_template
+
 
 from helper_functions import (
-    move_paycode_from_source_to_target,
     get_random_paycode,
-    load_streamlit_template,
-    read_leaderboard,
-    write_leaderboard,
-    update_leaderboard,
 )
 
-st.title("Paycode Helper")
+from leaderboard_utils import update_leaderboard
+
+def sidebar_navigation():
+    st.sidebar.title("Navigation")
+    options = st.sidebar.radio("Go to", ["Home", "Leaderboard"])
+    return options
 
 
 def fill_paycode_form():
@@ -35,70 +44,28 @@ def submit_paycode(paycode):
     st.success("Thank you!")
 
 
-# Leaderboard function
-def display_leaderboard():
-    leaderboard_json = read_leaderboard()
-    leaderboard = leaderboard_json["leaderboard"]
-
-    st.markdown("## Leaderboard")
-
-    for i, entry in enumerate(leaderboard):
-        if i == 0:
-            medal = "🥇"
-        elif i == 1:
-            medal = "🥈"
-        elif i == 2:
-            medal = "🥉"
-        else:
-            medal = ""
-
-        st.write(f"{medal} {entry['name']}: {entry['score']} documents")
-
 
 def main():
-    # Set up the layout with three columns
     streamlit_input_template = load_streamlit_template()
 
-    col1, col2, col3, col4 = st.columns(4)
+    choice = sidebar_navigation()
+
+    col1, col2, col3 = st.columns(3)
 
     # User input form based on the provided JSON structure
     with st.form(key="data_form"):
         with col1:
-            st.subheader("Data Entry Form")
-
             st.button(
                 "Pick Random Paycode",
                 on_click=get_random_paycode,
                 args=("paycodehelper-templates", "paycodehelper-processing"),
             )
-            user_name = st.text_input("Enter your name: ")
+            st.header("Data Entry Form")
 
-            if "paycode" in st.session_state:
-                prefilled_fields = st.session_state["paycode"]["prefilled"]
-
-                st.markdown("#### Paycode: ")
-                st.info(f'{prefilled_fields["paycode"]}')
-                st.markdown("#### Name: ")
-                st.info(f'{prefilled_fields["name"]}')
-                st.markdown("#### Type: ")
-                st.info(f'{prefilled_fields["type"]}')
-                st.markdown("#### Kommentar: ")
-                st.info(f'{prefilled_fields["kommentar"]}')
-
-            else:
-                st.info("No paycode selected", icon="ℹ")
-                st.markdown("#### Paycode Name: ")
-                st.info("")
-                st.markdown("#### Type: ")
-                st.info("")
-                st.markdown("#### Kommentar: ")
-                st.info("")
-
-            st.subheader("Information")
             st.toggle("AM-bidrag", value=False)
             for key in streamlit_input_template["text_area"]:
                 st.text_area(
-                    key, help=streamlit_input_template["text_area"][key]["help"]
+                    label=key, help=streamlit_input_template['text_area'][key]['help']
                 )
 
             # Lønart input field
@@ -117,31 +84,54 @@ def main():
 
         with col2:
 
-            st.header("Lønart information")
+            st.header("Lønart Information")
 
-            # TODO: Bools should be static information displayted in the middle column
-            st.subheader("lønbehandlingskategorier")
-            for key in streamlit_input_template["bools"]:
-                st.toggle(
-                    key,
-                    help=streamlit_input_template["bools"][key]["help"],
-                    value=streamlit_input_template["bools"][key]["default"],
-                    disabled=True,
+            user_name = st.text_input("Enter your name: ")
+
+            if "paycode" in st.session_state:
+                catalog_fields = st.session_state["paycode"]["catalog"]
+                st.text_input(
+                    label="Paycode", value=catalog_fields["paycode"], disabled=True,
                 )
+                st.text_input(
+                    label="Name", value=catalog_fields["name"], disabled=True,
+                )
+                st.text_input(
+                    label="Type", value=catalog_fields["type"], disabled=True
+                )
+                st.text_input(
+                    label="Kommentar", value=catalog_fields["kommentar"], disabled=True
+                )
+
+            else:
+                st.info("No paycode selected", icon="ℹ")
+
+            if "paycode" in st.session_state:
+                catalog = st.session_state["paycode"]["catalog"]
+                st.toggle("Pensionsgrundlag", value=catalog["Pensionsgrundlag"])
+                st.toggle("E-indkomst", value=catalog["E-indkomst"])
+                st.toggle("Ferieberettiget", value=catalog["Ferieberettiget"])
+                st.toggle("ATP-timer", value=catalog["ATP-timer"])
+                st.info(f"IL-typer: {catalog['IL-typer']}")
 
         submit_button = st.form_submit_button(label="Submit")
 
         if user_name and submit_button:
+            # TODO: add all inputs to paycode (update json)
             update_leaderboard(user_name)
-            # submit_paycode()
+            submit_paycode(st.session_state.paycode)
             st.success(f"Document submitted by {user_name}!")
 
     with col3:
+        st.header("User Input Summary")
+        st.write(
+            "AI Summary"
+        )
         st.header("AI Summary")
 
         # Update placeholders
         st.write(
-            """Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam molestie erat tortor, at mollis lorem iaculis ut. Aliquam erat volutpat. Sed mauris metus, congue ac quam ac, tincidunt sodales nulla. Nunc fermentum fringilla augue, sit amet mattis dolor finibus at. Etiam id feugiat diam, non imperdiet nisi. Cras vulputate suscipit tortor, a malesuada neque tristique id. Aliquam egestas, est eget pulvinar lacinia, tortor purus semper tortor, nec lacinia dolor sem a nisi. Vestibulum tincidunt quam magna, eu cursus justo ultrices quis. Vestibulum aliquet, eros in mollis sollicitudin, arcu odio finibus ante, vitae convallis lorem orci vitae orci."""
+            "AI Summary"
         )
 
         st.header("Sources")
@@ -152,9 +142,6 @@ def main():
         3. Source three: https://example.com/source3
         """
         )
-
-    with col4:
-        display_leaderboard()
 
 
 if __name__ == "__main__":
---
+++ leaderboard.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+from leaderboard_utils import display_leaderboard
+
+st.title("Lønart Leaderboard")
+
+display_leaderboard()
+
