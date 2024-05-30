a23b79f9a981b969fb395423a15326e33d582d13
lundberg-johan12 <johan.lundberg@visma.com>
2024-05-30 10:35:30 +0200

    cleanup of files not used

--- get_paycodes_from_excel.py
+++
@@ -1,9 +0,0 @@
-
-import polars as pl
-import numpy as np
-
-df = pl.read_excel("data/Paycodes Standard.xlsx", sheet_name="Lønartskatalog")
-
-df = df.select(["Navn", "Lønartnr", "Udskrivnings sekvens", "Type"])
-
-df.write_csv("data/paycodes.csv")
