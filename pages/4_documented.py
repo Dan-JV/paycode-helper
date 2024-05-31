46fdbbcee522ec18a2c9d31b260d95179a284cb7
dan <danjohansson98@gmail.com>
2024-05-31 13:00:46 +0200

    Implemeted editting capabaility

--- pages/4_documented.py
+++ pages/4_documented.py
@@ -1,5 +1,9 @@
 import streamlit as st
 import boto3
+from streamlit_tags import st_tags
+import json
+import ast
+
 
 st.set_page_config(
     page_title="Documented📂",
@@ -8,7 +12,7 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
-from streamlit_utils import sidebar_navigation
+from streamlit_utils import sidebar_navigation, load_streamlit_template
 from aws_helper_functions import list_available_paycodes, get_paycode
 from ai_summary import ai_summary
 
@@ -24,9 +28,6 @@ s3 = boto3.client("s3")
 
 def submit_paycode(paycode, key):
     s3.put_object(Body=paycode, Bucket="paycodehelper-documented", Key=key)
-    s3.delete_object(Bucket="paycodehelper-processing", Key=key)
-    st.write("Submitting paycode")
-    st.success("Thank you!")
 
 
 def main():
@@ -34,6 +35,8 @@ def main():
     st.title("Guide📖")
 
     sidebar_navigation()
+
+    streamlit_input_template = load_streamlit_template()
 
     paycode_list = get_paycode_list()
 
@@ -50,20 +53,6 @@ def main():
 
     col1, col2, col3 = st.columns(3)
 
-    ######################################################
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    # Everything below this point is broken, Gonna try and fix tomorrow
-    ########################################################################
-
-
-# XD THIS IS GOOD ****
-if 1 == 2:
     if "paycode" in st.session_state:
         with col2:
             st.button(
@@ -72,104 +61,105 @@ if 1 == 2:
                 args=(st.session_state["paycode"],),
             )
 
-            col1, col2, col3 = st.columns(3)
+        col1, col2, col3 = st.columns(3)
 
-            with col1:
-                st.header("Data Entry Form")
+        with col1:
+            st.header("Data Entry Form")
 
-                st.session_state["paycode"]["user_input"]["bools"] = st.toggle(
-                    "AM-bidrag", value=False
-                )
-                for key in streamlit_input_template["text_area"]:
-                    st.session_state["paycode"]["user_input"]["text_fields"][key] = (
-                        st.text_area(
-                            label=key,
-                            help=streamlit_input_template["text_area"][key]["help"],
-                        )
+            st.session_state["paycode"]["user_input"]["bools"] = st.toggle(
+                "AM-bidrag", value=False
+            )
+            for key in st.session_state["paycode"]["user_input"]["text_fields"]:
+                st.session_state["paycode"]["user_input"]["text_fields"][key] = (
+                    st.text_area(
+                        label=key,
+                        value=st.session_state["paycode"]["user_input"]["text_fields"][
+                            key
+                        ],
                     )
-
-                # Lønart input field
-                st.session_state["paycode"]["user_input"]["input"] = st.multiselect(
-                    "Lønart input(s)",
-                    options=streamlit_input_template["input"],
-                    help="Help us fill these",
                 )
 
-                tags = st_tags(
-                    label="Enter Keywords",
-                    text="Press enter to add more",
-                    value=[],
-                    maxtags=100,
-                )
+            # Lønart input field
+            st.session_state["paycode"]["user_input"]["input"] = st.multiselect(
+                "Lønart input(s)",
+                options=streamlit_input_template["input"],
+                default=st.session_state["paycode"]["user_input"]["input"],
+            )
 
-            with col2:
-                st.header("Lønart Information")
+            # Tags / keywords
+            st.session_state["paycode"]["user_input"]["text_fields"]["tags"] = st_tags(
+                label="Enter Keywords",
+                text="Press enter to add more",
+                value=ast.literal_eval(
+                    st.session_state["paycode"]["user_input"]["text_fields"]["tags"]
+                ),
+                maxtags=100,
+            )
 
-                catalog = st.session_state["paycode"]["catalog"]
-                st.text_input(
-                    label="Paycode",
-                    value=catalog["paycode"],
-                    disabled=True,
-                )
-                st.text_input(
-                    label="Name",
-                    value=catalog["name"],
-                    disabled=True,
-                )
-                st.text_input(label="Type", value=catalog["type"], disabled=True)
-                st.text_input(
-                    label="Kommentar", value=catalog["kommentar"], disabled=True
-                )
-                st.info(f"Pensionsgrundlag: {catalog['Pensionsgrundlag']}")
-                st.info(f"E-indkomst: {catalog['E-indkomst']}")
+        with col2:
+            st.header("Lønart Information")
 
-                for key in ["Ferieberettiget", "ATP-timer"]:
-                    checkbox_container = st.container()
+            catalog = st.session_state["paycode"]["catalog"]
+            st.text_input(
+                label="Paycode",
+                value=catalog["paycode"],
+                disabled=True,
+            )
+            st.text_input(
+                label="Name",
+                value=catalog["name"],
+                disabled=True,
+            )
+            st.text_input(label="Type", value=catalog["type"], disabled=True)
+            st.text_input(label="Kommentar", value=catalog["kommentar"], disabled=True)
+            st.info(f"Pensionsgrundlag: {catalog['Pensionsgrundlag']}")
+            st.info(f"E-indkomst: {catalog['E-indkomst']}")
 
-                    checked = catalog[key]
-                    st.session_state["paycode"]["catalog"][key] = checked
+            for key in ["Ferieberettiget", "ATP-timer"]:
+                checkbox_container = st.container()
 
-                    checked = "✅" if catalog[key] == "Ja" else "❌"
+                checked = catalog[key]
+                st.session_state["paycode"]["catalog"][key] = checked
 
-                    st.write(f"{key}: {checked}")
+                checked = "✅" if catalog[key] == "Ja" else "❌"
 
-                st.info(f"IL-typer: {catalog['IL-typer']}")
+                st.write(f"{key}: {checked}")
 
-            with col3:
-                st.header("User Input Summary")
+            st.info(f"IL-typer: {catalog['IL-typer']}")
 
-                if "ai_summary" in st.session_state:
-                    st.write(st.session_state["ai_summary"])
+        with col3:
+            st.header("User Input Summary")
 
-                st.header("AI Summary of Guides")
+            if "ai_summary" in st.session_state:
+                st.write(st.session_state["ai_summary"])
 
-                # Update placeholders
-                st.write("AI Summary")
+            st.header("AI Summary of Guides")
 
-                st.header("Sources")
-                st.write(
-                    """
-                1. Source one: https://example.com/source1
-                2. Source two: https://example.com/source2
-                3. Source three: https://example.com/source3
+            # Update placeholders
+            st.write("AI Summary")
+
+            st.header("Sources")
+            st.write(
                 """
+            1. Source one: https://example.com/source1
+            2. Source two: https://example.com/source2
+            3. Source three: https://example.com/source3
+            """
+            )
+        # TODO: Potential problem: if the user presses "enter" after filling out a field, the form will be submitted!
+        st.session_state["submit_button"] = st.button(label="Save edits")
+
+        if st.session_state.user_name:
+            if st.session_state["submit_button"]:
+                key = (
+                    f"paycode_{st.session_state['paycode']['catalog']['paycode']}.json"
                 )
-            # TODO: Potential problem: if the user presses "enter" after filling out a field, the form will be submitted!
-            st.session_state["submit_button"] = st.form_submit_button(label="Submit")
+                submit_paycode(
+                    json.dumps(st.session_state.paycode, ensure_ascii=False), key
+                )
 
-            if st.session_state.user_name:
-                if st.session_state["submit_button"]:
-                    update_leaderboard(st.session_state.user_name)
-                    key = f"paycode_{st.session_state['paycode']['catalog']['paycode']}.json"
-                    submit_paycode(
-                        json.dumps(st.session_state.paycode, ensure_ascii=False), key
-                    )
-                    st.success(f"Document submitted by {st.session_state.user_name}!")
-
-                    st.session_state.paycode = None
-
-    else:
-        st.info("No paycode selected", icon="ℹ")
+                st.success("Paycode has been edited and saved!🚀")
+                st.session_state.paycode = None
 
 
 if __name__ == "__main__":  #
