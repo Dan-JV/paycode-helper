3d0404bbec35da0d586d60e0ec3cf45384e5f62d
Dan <dan.johansson@visma.com>
2024-06-03 17:15:31 +0200

    Added check for existing paycodes

--- src/pages/4_documented.py
+++ src/pages/4_documented.py
@@ -39,74 +39,79 @@ def main():
 
     paycode_list = get_paycode_list()
 
-    paycode = st.selectbox(
-        "Select a paycode",
-        paycode_list,
-    )
-    paycodenr = paycode.split("_")[1].split(".")[0]
-    st.session_state["paycodenr"] = paycodenr
+    # check if there are any paycodes available
+    if paycode_list:
 
-    # returns the paycode as a dictionary and a session state
-    # TODO: make this paycode in session_state different from the one on the main page
-    paycode = get_paycode(bucket="paycodehelper-documented", key=paycode)
-    st.session_state["paycode"] = paycode
-
-    # st.json(st.session_state["paycode"], expanded=False)
-
-    file_path = "src/templates/field_templates.yaml"
-    template = load_template(file_path).model_dump()
-    form_template = template["form_template"]
-    feedback_template = template["feedback_template"]
-
-    col1, col2, col3, col4, col5 = st.columns(5)
-
-    form_template["areas"] = paycode["areas"]
-
-    with col2:
-        st.button(
-            "Generate AI Summary🤖",
-            on_click=ai_summary,
-            args=(st.session_state["paycode"],),
+        paycode = st.selectbox(
+            "Select a paycode",
+            paycode_list,
         )
-        if "ai_summary" in st.session_state:
-            form_template["areas"][2]["fields"][0]["input"] = st.session_state[
-                "ai_summary"
-            ]
-    with col3:
-        with st.popover("Feedback😅"):
-            with st.form(key="feedback_form", clear_on_submit=True):
-                for index, field in enumerate(template["feedback_template"]["fields"]):
+        paycodenr = paycode.split("_")[1].split(".")[0]
+        st.session_state["paycodenr"] = paycodenr
 
-                    if field["name"] == "user_name":
-                        field["input"] = st.session_state.user_name
-                        disabled = True
-                    elif field["name"] == "paycodenr":
-                        field["input"] = st.session_state.paycodenr
-                        disabled = True
-                    else:
-                        disabled = False
+        # returns the paycode as a dictionary and a session state
+        # TODO: make this paycode in session_state different from the one on the main page
+        paycode = get_paycode(bucket="paycodehelper-documented", key=paycode)
+        st.session_state["paycode"] = paycode
 
-                    create_field(field, disabled)
+        # st.json(st.session_state["paycode"], expanded=False)
 
-                paycodenr = st.session_state.get("paycodenr", "")
-                user_name = st.session_state.get("user_name", "")
-                email = feedback_template["fields"][2]["input"]
-                feedback = feedback_template["fields"][3]["input"]
-                feedback_dict = {
-                    "paycodenr": paycodenr,
-                    "name": user_name,
-                    "email": email,
-                    "feedback": feedback,
-                }
+        file_path = "src/templates/field_templates.yaml"
+        template = load_template(file_path).model_dump()
+        form_template = template["form_template"]
+        feedback_template = template["feedback_template"]
 
-                key = f'{user_name}_{datetime.now().strftime("%Y-%m-%d-%H:%M:%S")}_paycode_{paycodenr}.json'
-                submitted = st.form_submit_button("Submit")
+        col1, col2, col3, col4, col5 = st.columns(5)
 
-                if submitted:
-                    upload_feedback(feedback_dict, key=key)
-                    st.success("Thank you for your feedback!")
+        form_template["areas"] = paycode["areas"]
 
-    create_paycode_form(form_template)
+        with col2:
+            st.button(
+                "Generate AI Summary🤖",
+                on_click=ai_summary,
+                args=(st.session_state["paycode"],),
+            )
+            if "ai_summary" in st.session_state:
+                form_template["areas"][2]["fields"][0]["input"] = st.session_state[
+                    "ai_summary"
+                ]
+        with col3:
+            with st.popover("Feedback😅"):
+                with st.form(key="feedback_form", clear_on_submit=True):
+                    for index, field in enumerate(
+                        template["feedback_template"]["fields"]
+                    ):
+
+                        if field["name"] == "user_name":
+                            field["input"] = st.session_state.user_name
+                            disabled = True
+                        elif field["name"] == "paycodenr":
+                            field["input"] = st.session_state.paycodenr
+                            disabled = True
+                        else:
+                            disabled = False
+
+                        create_field(field, disabled)
+
+                    paycodenr = st.session_state.get("paycodenr", "")
+                    user_name = st.session_state.get("user_name", "")
+                    email = feedback_template["fields"][2]["input"]
+                    feedback = feedback_template["fields"][3]["input"]
+                    feedback_dict = {
+                        "paycodenr": paycodenr,
+                        "name": user_name,
+                        "email": email,
+                        "feedback": feedback,
+                    }
+
+                    key = f'{user_name}_{datetime.now().strftime("%Y-%m-%d-%H:%M:%S")}_paycode_{paycodenr}.json'
+                    submitted = st.form_submit_button("Submit")
+
+                    if submitted:
+                        upload_feedback(feedback_dict, key=key)
+                        st.success("Thank you for your feedback!")
+
+        create_paycode_form(form_template)
 
 
 if __name__ == "__main__":  #
